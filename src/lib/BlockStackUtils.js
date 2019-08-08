e58fdcf2c70c79ee05844871eae789944e768206
Michael Ogezi <okibeogezi@outlook.com>
2019-08-08 01:40:12 +0100

    Initial Commit

--- src/lib/BlockStackUtils.js
+++ src/lib/BlockStackUtils.js
@@ -10,23 +10,23 @@ export default class BlockStackUtils {
   }
 
   static isSignedInOrPending (that) {
-    return true;
-    // const session = that.userSession;
+    // return true;
+    const session = that.userSession;
 
-    // if (session.isUserSignedIn()) {
-    //   console.log('User Signed In');
-    //   return true;
-    // }
-    // else if (!session.isUserSignedIn() && session.isSignInPending()) {
-    //   console.log('User Sign In Pending');
-    //   session.handlePendingSignIn()
-    //     .then(user => console.log('User Data', user));
+    if (session.isUserSignedIn()) {
+      console.log('User Signed In');
+      return true;
+    }
+    else if (!session.isUserSignedIn() && session.isSignInPending()) {
+      console.log('User Sign In Pending');
+      session.handlePendingSignIn()
+        .then(user => console.log('User Data', user));
       
-    //   return true;
-    // }
-    // else {
-    //   return false;
-    // }
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   static signIn (that) {
